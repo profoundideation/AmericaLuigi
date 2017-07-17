2ccf0f078bf6026145f3ea1c98fb500e097ef8b0
Daniel Lim <hbt@markhamboards.com>
2017-07-17 15:15:56 -0400

    Add Index.
    Add Index.

---
+++ index.php
@@ -0,0 +1,248 @@
+<?php 
+     /* 
+     header('Location: http://americaluigi.com/2017'); 
+	 exit();
+	 */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Welcome to America Luigi</title>
+<meta name="description" content="Welcome to America Luigi, I am delighted to visit your homes or Restaurants and share our intimate cooking experience with the rest of the world." />
+
+<script type="text/JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+<link href="america.css" rel="stylesheet" type="text/css" />
+<script src="//load.sumome.com/" data-sumo-site-id="9e0cbad8eb14a6837be425217c7d204283621c529f049809ef4e1fc53dfcc976" async="async"></script>
+</head>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56296546-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<body onload="MM_preloadImages('images/promotional_serv_m.jpg','images/resto_wine_m.jpg','images/clothing_m.jpg','images/home_style_m.jpg')">
+<p>&nbsp;</p>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center" class="main_bg"><table width="1000" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><img src="images/logo1.jpg" width="1000" height="213" /></td>
+      </tr>
+      <tr>
+        <td align="center" bgcolor="#F3D308"><table width="1000" border="0" cellspacing="0" cellpadding="0">
+			<tr>
+			<td width="100" align="left"><a href="http://www.facebook.com/pages/America-Luigi/166224766733961" target="_blank" onmouseover="MM_swapImage('Image47','','images/facelogo_m.jpg',1)" onmouseout="MM_swapImgRestore()"><img src="images/facelogo.jpg" alt="Follow me on Facebook" name="Image47" width="28" height="28" border="0" id="Image47" /></a></td>
+
+            <td align="center" style="width: 200px;" title="ABOUT US" alt="ABOUT US" ><a href="index.php"><span style="font-size: large; color: #000; padding: 0px 30px 0px 0px;">ABOUT</span></a></td>
+
+            <td align="center" style="width: 200px;"><img src="images/line1.jpg" width="1" height="30" /></td>
+
+			<td align="center" style="width: 200px;"><a href="mobile_app.php"><span style="font-size: large; color: #000; padding: 0px 30px 0px 30px;">GAMES</span></a></td>
+
+            <td align="center" style="width: 200px;"><img src="images/line1.jpg" width="1" height="30" /></td>
+
+            <td align="center" style="width: 200px;"><a href="youtube.php"><span style="font-size: large; color: #000; padding: 0px 30px 0px 30px;">YOUTUBE</span></a></td>
+
+            <td align="center" style="width: 200px;"><img src="images/line1.jpg" width="1" height="30" /></td>
+            
+					<!-- <li><a href="cornucopia.php">Cornucopia</a></li> 
+					<li><a href="gallery.php">Gallery</a></li> -->
+					
+			<td align="center" style="width: 200px;"><a href="gallery.php"><span style="font-size: large; color: #000; padding: 0px 30px 0px 30px;">GALLERY</span></a></td>
+
+			<td align="center" style="width: 200px;"><img src="images/line1.jpg" width="1" height="30" /></td>
+
+			<td align="center" style="width: 200px;"><a href="novelty_items.php" alt="NOVELTY ITEMS" title="NOVELTY ITEMS"><span style="font-size: large; color: #000; padding: 0px 0px 0px 30px;">COLLECTIBLES</span></a></td>
+
+          <td width="100" align="center">&nbsp;</td>
+          </tr>
+        </table></td>
+      </tr>
+<tr>
+            <td align="right" valign="top" style="background-color:#FFFFFF">
+					  </td>
+          </tr>
+      <tr>
+        <td align="right" valign="top" style="background:url(images/bg2.jpg) center top no-repeat; height:1000px; padding-right:70px; margin-top:100px"><table width="930" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="285" align="left" valign="top"><table width="50%" border="0" cellspacing="0" cellpadding="0">
+              <!-- <tr>
+                <td><img src="images/twitterlogo.jpg" width="182" height="47" vspace="2" border="0" /></td>
+              </tr> -->
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td align="center"><marquee width="165px" style="color:#333;" loop="true" direction="up" scrollamount="2"><center>
+<strong><br /><br /> <img src="images/fruits.png" alt="Fruits" class="border_nm" title="Fruits" /><br /><br /><img src="images/veg.png" alt="Vegetables" class="border_nm" Title="Vegetables"/><br /><br /><img src="images/egg.png" alt="Eggs" class="border_nm" title="Eggs"><br /><br /><img src="images/fish.png" alt="Fish" class="border_nm"  title="Fish"/> </strong>
+                </center></marquee></td>
+              </tr>
+            </table></td>
+            <td width="715" align="right" valign="top"  ><table width="100%" border="0" cellspacing="0" cellpadding="0">
+             <tr>
+               <td align="right"><table width="635" border="0" cellspacing="0" cellpadding="0">
+                 <tr>
+                   <td height="20" ></td>
+                 </tr>
+                 <tr>
+                      <td ><marquee behavior="scroll" direction="left" scrollamount="4" style="font-size:12px; color:#480707; font-family:Georgia, 'Times New Roman', Times, serif;">
+                      Advertise your product with <strong>America Luigi</strong>&nbsp;&nbsp;&nbsp;&nbsp;<img src="images/newlogo.jpg" style="vertical-align:middle;" /> &nbsp;&nbsp;&nbsp;&nbsp;Advertise your product with <strong>America Luigi</strong>&nbsp;&nbsp;&nbsp;&nbsp;<img src="images/newlogo.jpg" style="vertical-align:middle;" />&nbsp; &nbsp;Advertise your product with <strong>America Luigi</strong>&nbsp;&nbsp;&nbsp;&nbsp;<img src="images/newlogo.jpg" style="vertical-align:middle;" /> </marquee></td>
+                  </tr>
+               </table></td></tr> <tr>
+                <td align="right" valign="top" ><div id="google_translate_element" style="margin-right:5px; margin-top:20px;"></div><script>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'sq',
+    includedLanguages: 'af,sq,ar,be,bg,ca,zh-CN,zh-TW,hr,cs,da,nl,en,et,tl,fi,fr,gl,de,el,ht,iw,hi,hu,is,id,ga,it,ja,ko,lv,lt,mk,ms,mt,no,fa,pl,pt,ro,ru,sr,sk,sl,es,sw,sv,th,tr,uk,vi,cy,yi'
+  }, 'google_translate_element');
+}
+</script><script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script></td>
+              </tr>
+              <tr>
+                <td align="right"><table width="620" border="0" align="right" cellpadding="0" cellspacing="0">
+
+                  <tr>
+                    <td align="left"><img src="images/welcom.jpg" width="460" height="110"  style="padding-top:15px; padding-bottom:9px"/></td>
+                  </tr>
+                  <tr>
+                    <td height="89" align="left"><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="615" height="100" id="Luigi2" align="middle">
+                      <param name="allowScriptAccess" value="sameDomain" />
+                      <param name="movie" value="Luigi.swf" />
+                      <param name="quality" value="high" />
+                      <param name="wmode" value="transparent" />
+                      <param name="bgcolor" value="#ffffff" />
+                      <embed src="Luigi.swf" quality="high" wmode="transparent" bgcolor="#ffffff" width="615" height="100" name="Luigi" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+                      </embed>
+                      </embed>
+                    </object></td>
+                  </tr>
+                  <tr>
+                    <td align="left"><div> <img src="http://americaluigi.com/2017/images/Am-Pix-8.jpg" width="300" align="left" class="border" style="margin-right:8px; margin-top:5px;" />
+					<!-- images/about-americaluigi.png -->
+                      <!--<h1> Fun Foods</h1>-->
+                      <div align="justify" style="padding-top:2px; line-height:25px;">
+                        <h4  style=" line-height:25px;">
+							<p>America Luigi is a fictional chef, featured on YouTube with his mini-me's Squeegee & Weegee.</p>
+							<p>Hi I'm the owner/founder of America Luigi, 1st generation North American born from Italian Immigrants.  Along with Italians, other ethnic groups who come to North America for a better quality of life, bringing their crafted skills and traditional Homestyle recipes, making North America one of the largest melting pots of ethnic dishes.<br /><br /> I can be reached at <a href="mailto:info@americaluigi.com">info @americaluigi.com</a> or <a href="https://twitter.com/americaluigi" target="_blank">Twitter @AmericaLuigi</a>
+							</p>
+						</h4>
+                      </div>
+                    </div></td>
+                  </tr>
+                  <tr>
+                    <td height="10px"></td>
+                  </tr>
+                  <tr>
+                    <td align="center"><img src="images/line2.jpg" width="353" height="27" /></td>
+                  </tr>
+
+                  <tr>
+                    <td align="center">&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td align="left" valign="top"><table width="620" border="0" cellspacing="0" cellpadding="0">
+                      <tr>
+                        <td width="261" align="left" valign="top">
+						<img src="http://www.americaluigi.com/images/AM-Mail.png" width="60" />
+						<img src="images/contact.jpg" width="180" height="42" />
+						<br />
+
+						<a href="mailto:info@americaluigi.com"><span style="font-size: x-large;">info@americaluigi.com</span></a>
+						<br />
+						<p>
+						<?php /* <img src="images/AM-Icon_App.png" width="90px;" />	 */ ?>
+<a href="https://play.google.com/store/apps/details?id=americaluigi.com" rel="muse" target="_blank">
+  &nbsp;<img alt="Android app on Google Play" width="150px"
+       src="https://developer.android.com/images/brand/en_app_rgb_wo_60.png" />
+</a>
+<a href="https://itunes.apple.com/ca/app/spinning-pizza/id931424825?mt=8" rel="muse" target="_blank">
+  <img alt="Android app on the Apple App Store" width="150px"
+       src="images/app-store-button.png" />
+	   <br /><br /><br />
+</a>
+<p><br /><br /><br /></p>
+
+
+</p>
+						</td>
+                        <td width="359" align="left" valign="middle">
+						<a href="mobile_app.php"><img src="http://www.americaluigi.com/images/LuigiMobileApp.png" width="250px"align="center" class="borderX" style="margin-left:8px; margin-top:1px; padding-top:8px" /></a>
+<p><br /><br /><br /></p>
+
+						</td>
+                      </tr>
+                      <tr>
+                        <td align="left" valign="top" style="padding-left:16px; padding-top:20px;"></td>
+                        <td align="left"><div>
+                        </div></td>
+                      </tr>
+                      <tr>
+                        <td height="10" align="center" valign="top"></td>
+                        <td height="10" align="left"></td>
+                      </tr>
+                    </table></td>
+                  </tr>
+
+
+                </table></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+
+      <tr>
+        <td height="70" bgcolor="#493015">
+			<div class="menu">
+				<ul>
+					<li><a href=".">About Us</a></li>
+					<li><a href="mobile_app.php">Mobile Game Apps.</a></li>
+					<li><a href="youtube.php">YouTube</a></li>
+					<!-- <li><a href="cornucopia.php">Cornucopia</a></li> -->
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="novelty_items.php">Novelty Items</a></li>
+					<li>America Luigi Registered Trademark  &nbsp; &nbsp; &nbsp; All Rights Reserved.  </li>
+				</ul>
+			<div align="right">
+<abbr title="Web Development">Web Dev.</abbr> & <abbr title="Search Engine Optimization">SEO<abbr> by <a href="http://ProfoundIdeation.com" style="text-decoration:none" rel="contact met co-worker" title="Profound Ideation Inc. Web Development & SEO Toronto" alt="Profound Ideation Inc. SEO & Web Dev. Toronto"><img src="http://torontoseokeywords.com/wp-content/uploads/2012/10/ProfoundIdeation-Logo.png" title="Profound Ideation Inc. Web Development & SEO Toronto" alt="Profound Ideation Inc. SEO & Web Dev. Toronto" width="15px" height="15px" /></a> <a style="text-decoration:none" href="http://ProfoundIdeation.com" title="Profound Ideation Inc. Web Development & SEO Toronto" alt="Profound Ideation Inc. SEO & Web Dev. Toronto" rel="contact met co-worker" target="_blank"><span class="fn">Profound Ideation Inc.</span></a>
+			</div>
+		</td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+
+</div>
+</body>
+</html>
